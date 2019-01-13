638437bb99380dd92d552718bbe3d696b5c3c125
Topher Winward <winwardo@gmail.com>
2019-01-13 15:29:19 +0000

    Sorted

--- src/poker/mod.rs
+++ src/poker/mod.rs
@@ -1,21 +1,10 @@
-// pub mod compare_hands;
-// pub mod comparison_result;
-
-mod compare_hands;
-
 mod serializer;
 pub use self::serializer::deserialize;
 pub use self::serializer::serialize;
 
 mod card;
-// pub use self::card::Card;
-// pub use self::card_rank::CardRank;
-
 mod card_rank;
-// pub use self::card_rank::CardRank;
-
+mod compare_hands;
 mod comparison_result;
 mod hand;
 mod suits;
-
-// pub use self::suits::Suit;
