6ee6b8e036c341a66525fe342a0e64357365fe26
Topher Winward <winwardo@gmail.com>
2019-01-13 14:57:21 +0000

    Modules!

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,11 @@ fn main() {
 
 mod poker;
 
-enum CardRank {
-    Two = 2,
-    Three = 3,
-    Four = 4,
-    Five = 5,
-    Six = 6,
-    Seven = 7,
-    Eight = 8,
-    Nine = 9,
-    Ten = 10,
-    Jack = 11,
-    Queen = 12,
-    King = 13,
-    Ace = 14,
-}
+use self::poker::*;
 
 struct Card {
-    rank: CardRank,
-    suit: poker::suits::Suit,
+    rank: card_rank::CardRank,
+    suit: suits::Suit,
 }
 
 struct Hand {
