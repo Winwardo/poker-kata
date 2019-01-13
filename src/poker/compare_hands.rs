28515b669ecde4b75c3ac0f1dc0e0e46e267c4da
Topher Winward <winwardo@gmail.com>
2019-01-13 19:14:47 +0000

    First test

--- src/poker/compare_hands.rs
+++ src/poker/compare_hands.rs
@@ -8,11 +8,41 @@ pub struct CompareHands {
 }
 
 impl CompareHands {
-
     pub fn compare(&self) -> ComparisonResult {
-        ComparisonResult {
-            winner: Players::Black,
-            win_type: WinType::HighCard,
+        ComparisonResult { winner: None }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use super::super::card::*;
+    use super::super::card_rank::*;
+    use super::super::suits::*;
+
+    #[test]
+    fn same_hands_no_winner() {
+        let input = CompareHands {
+            black: Hand::new(vec![
+                Card::new(CardRank::Two, Suit::Hearts),
+                Card::new(CardRank::Three, Suit::Hearts),
+                Card::new(CardRank::Four, Suit::Hearts),
+                Card::new(CardRank::Five, Suit::Hearts),
+                Card::new(CardRank::Six, Suit::Hearts),
+            ])
+            .unwrap(),
+            white: Hand::new(vec![
+                Card::new(CardRank::Two, Suit::Clubs),
+                Card::new(CardRank::Three, Suit::Clubs),
+                Card::new(CardRank::Four, Suit::Clubs),
+                Card::new(CardRank::Five, Suit::Clubs),
+                Card::new(CardRank::Six, Suit::Clubs),
+            ])
+            .unwrap(),
         }
+        .compare();
+
+        assert_eq!(input, ComparisonResult { winner: None });
     }
 }
--- src/poker/comparison_result.rs
+++ src/poker/comparison_result.rs
@@ -1,13 +1,21 @@
+#[derive(Debug, PartialEq)]
 pub enum Players {
     Black,
     White,
 }
 
+#[derive(Debug, PartialEq)]
 pub enum WinType {
     HighCard,
 }
 
+#[derive(Debug, PartialEq)]
+pub struct Winner {
+    player: Players,
+    win_type: WinType,
+}
+
+#[derive(Debug, PartialEq)]
 pub struct ComparisonResult {
-    pub winner: Players,
-    pub win_type: WinType,
+    pub winner: Option<Winner>,
 }
