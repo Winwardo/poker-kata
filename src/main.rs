adaf31cac113635207d806f00734bf6524d8b4c0
Topher Winward <winwardo@gmail.com>
2019-01-13 14:26:20 +0000

    Outer tests

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ fn main() {
 }
 
 fn add(a: u8, b: u8) -> u8 {
-    a+b
+    a + b
 }
 
 fn poker_hand(input: &str) -> String {
@@ -13,13 +13,29 @@ fn poker_hand(input: &str) -> String {
 #[cfg(test)]
 mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
+    extern crate rstest;
+    use rstest::rstest_parametrize;
+
     use super::*;
 
-    #[test]
-    fn high_level_example_1() {
-        assert_eq!(
-            poker_hand("Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH"),
-            String::from("White wins. - with high card: Ace")
-        );
+    #[rstest_parametrize(
+        expected,
+        input,
+        case(
+            "White wins. - with high card: Ace",
+            "Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH"
+        ),
+        case(
+            "Black wins. - with full house: 4 over 2",
+            "Black: 2H 4S 4C 2D 4H  White: 2S 8S AS QS 3S"
+        ),
+        case(
+            "Black wins. - with high card: 9",
+            "Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C KH"
+        ),
+        case("Tie.", "Black: 2H 3D 5S 9C KD  White: 2D 3H 5C 9S KH")
+    )]
+    fn poker_hand_test(expected: &str, input: &str) {
+        assert_eq!(expected, poker_hand(input));
     }
 }
